b3ddc4170e84848c73216dad279781c12913f8a3
npapon <npapon@live.fr>
2022-06-13 13:23:32 +0200

    13/06/2022 V1

--- src/main/java/fr/stepbysteptestapi/DemoApplication.java
+++ src/main/java/fr/stepbysteptestapi/DemoApplication.java
@@ -1,18 +1,15 @@
 package fr.stepbysteptestapi;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import fr.stepbysteptestapi.model.Utilisateur_Persiste;
-import fr.stepbysteptestapi.service.Utilisateur_Persiste_Service;
-
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
-	@Autowired
-	private Utilisateur_Persiste_Service utilisateur_persiste_service;
+	/*
+	 * @Autowired private Utilisateur_Persiste_Service utilisateur_persiste_service;
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -21,12 +18,13 @@ public class DemoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("hello world");
-		Utilisateur_Persiste utilisateur_test = new Utilisateur_Persiste();
-		utilisateur_test.setLogin("test_login");
-		utilisateur_test.setMot_de_passe("test_mdp");
-		utilisateur_test.setEmail("test email");
-		utilisateur_persiste_service.saveUtilisateur(utilisateur_test);
+		/*
+		 * System.out.println("hello world"); Utilisateur_Persiste utilisateur_test =
+		 * new Utilisateur_Persiste(); utilisateur_test.setLogin("test_login");
+		 * utilisateur_test.setMot_de_passe("test_mdp");
+		 * utilisateur_test.setEmail("test email");
+		 * utilisateur_persiste_service.saveUtilisateur(utilisateur_test);
+		 */
 
 	}
 
---
+++ src/main/java/fr/stepbysteptestapi/controller/Utilisateur_Persiste_Controller.java
@@ -0,0 +1,88 @@
+package fr.stepbysteptestapi.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.stepbysteptestapi.model.Utilisateur_Persiste;
+import fr.stepbysteptestapi.service.Utilisateur_Persiste_Service;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class Utilisateur_Persiste_Controller {
+
+	@Autowired
+	private Utilisateur_Persiste_Service utilisateur_Persiste_Service;
+
+	@GetMapping("/utilisateur")
+	public Iterable<Utilisateur_Persiste> getAllUtilisateur() {
+		return utilisateur_Persiste_Service.getAllUtilisateur();
+	}
+
+	@GetMapping("/utilisateur/{id_utilisateur}")
+	public Utilisateur_Persiste getUtilisateur(@PathVariable("id_utilisateur") final Long id_utilisateur) {
+		Optional<Utilisateur_Persiste> optionalUtilisateur = utilisateur_Persiste_Service
+				.getUtilisateur(id_utilisateur);
+		if (optionalUtilisateur.isPresent()) {
+			return optionalUtilisateur.get();
+		} else {
+			return null;
+		}
+	}
+
+	@PostMapping("/utilisateur")
+	public Utilisateur_Persiste createUtilisateur(@RequestBody Utilisateur_Persiste utilisateur) {
+		return utilisateur_Persiste_Service.saveUtilisateur(utilisateur);
+	}
+
+	@PutMapping("/utilisateur/{id_utilisateur_a_updater}")
+
+	public Utilisateur_Persiste updateEmployee(
+			@PathVariable("id_utilisateur_a_updater") final Long id_utilisateur_a_updater,
+			@RequestBody Utilisateur_Persiste utilisateurAvecLesMisesAJour) {
+
+		Optional<Utilisateur_Persiste> optionalUtilisateurDuEndPoint = utilisateur_Persiste_Service
+				.getUtilisateur(id_utilisateur_a_updater);
+
+		if (optionalUtilisateurDuEndPoint.isPresent()) {
+			Utilisateur_Persiste utilisateurEndPoint = optionalUtilisateurDuEndPoint.get();
+
+			String loginAMettreAJour = utilisateurAvecLesMisesAJour.getLogin();
+			if (loginAMettreAJour != null) {
+				utilisateurEndPoint.setLogin(loginAMettreAJour);
+			}
+
+			String mot_de_passeAMettreAJour = utilisateurAvecLesMisesAJour.getMot_de_passe();
+			if (mot_de_passeAMettreAJour != null) {
+				utilisateurEndPoint.setMot_de_passe(mot_de_passeAMettreAJour);
+
+			}
+			String emailAMettreAJour = utilisateurAvecLesMisesAJour.getEmail();
+			if (emailAMettreAJour != null) {
+				utilisateurEndPoint.setEmail(emailAMettreAJour);
+			}
+
+			utilisateur_Persiste_Service.saveUtilisateur(utilisateurEndPoint);
+
+			return utilisateurEndPoint;
+
+		} else {
+
+			return null;
+		}
+	}
+
+	@DeleteMapping("/utilisateur/{id_utilisateur_a_supprimer}")
+	public void deleteUtilsateur(@PathVariable("id_utilisateur_a_supprimer") final Long id_utilisateur_a_supprimer) {
+		utilisateur_Persiste_Service.deleteUtilisateur(id_utilisateur_a_supprimer);
+	}
+
+}
